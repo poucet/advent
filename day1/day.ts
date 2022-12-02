0093b6bea6b8298b14bf808a195ace77178e5a46
Chris Poucet <poucet@google.com>
2022-12-02 14:49:57 +0100

    Advent of code 2022 - Day 1

---
+++ day1/day.ts
@@ -0,0 +1,46 @@
+import * as fs from 'fs';
+
+function readFile(path: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+}
+
+function parse(data: string): Array<Array<number>> {
+  const result: Array<Array<number>> = [[]];
+  for (const line of data.split('\n')) {
+    if (line == '') {
+      result.push([]);
+    } else {
+      result[result.length - 1].push(parseInt(line));
+    }
+  }
+  return result;
+}
+
+function sum(numbers: Array<number>): number {
+  if (numbers.length === 0) return 0;
+  if (numbers.length === 1) return numbers[0];
+  return numbers.reduce((p, c) => p + c)
+}
+
+function maxL(numbers: Array<number>) {
+  if (numbers.length === 0) return 0;
+  if (numbers.length === 1) return numbers[0];
+  return numbers.reduce((p, c) => Math.max(p, c));
+}
+
+async function main() {
+  const fileData = await readFile('day1/input.txt');
+  const data = parse(fileData).map(sum);
+  data.sort();
+  console.log(sum(data.slice(data.length - 3)));
+}
+
+main()
