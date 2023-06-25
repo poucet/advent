689d0d57c125afe5b71153e8c08c6eacc9fdf94f
Chris Poucet <christophe.poucet@gmail.com>
2023-06-25 23:33:22 +0200

    Added tests and diagonal neighbors for grid.

--- 2021/common/src/lib.rs
+++ 2021/common/src/lib.rs
@@ -59,25 +59,84 @@ impl Grid {
     if c > 0 {
       nbors.push((r, c - 1));
     }
+    if c < self.num_columns() - 1 {
+      nbors.push((r, c + 1));
+    }
     if r < self.num_rows() - 1 {
       nbors.push((r+1, c));
     }
+
+    nbors
+  }
+
+  // Includes diagonal neighbors.
+  pub fn all_neighbors(&self, r: usize, c: usize) -> Vec<Pos> {
+    let mut nbors = Vec::new();
+    if r > 0 && c > 0{
+      nbors.push((r - 1, c - 1));
+    }
+    if r > 0 {
+      nbors.push((r - 1, c));
+    }
+    if r > 0 && c < self.num_columns() - 1 {
+      nbors.push((r - 1, c + 1));
+    }
+    if c > 0 {
+      nbors.push((r, c - 1));
+    }
     if c < self.num_columns() - 1 {
       nbors.push((r, c + 1));
+    }
+    if r < self.num_rows() - 1 && c > 0{
+      nbors.push((r + 1, c - 1));
+    }
+
+    if r < self.num_rows() - 1 {
+      nbors.push((r + 1, c));
+    }
+
+    if r < self.num_rows() - 1 && c < self.num_columns() - 1 {
+      nbors.push((r + 1, c + 1));
     }
     nbors
   }
 }
 
 impl Index<usize> for Grid {
-    type Output = Vec<u32>;
-  
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.0[index]
-    }
+  type Output = Vec<u32>;
+
+  fn index(&self, index: usize) -> &Self::Output {
+      &self.0[index]
+  }
 }
   
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
+
+  #[test]
+  fn grid_parses() {
+    assert_eq!(
+      vec![
+        vec![1, 2, 3],
+        vec![4, 5, 6],
+        vec![7, 8, 9],
+      ],
+      Grid::parse("123\n456\n789").0
+    )
+  }
+
+  #[test]
+  fn it_neighbors() {
+    let g = Grid::parse("123\n456\n789");
+    assert_eq!(
+      vec![(0, 1), (1, 0)],
+       g.neighbors(0, 0)
+    );
+    assert_eq!(
+      vec![(0, 1), (1, 0), (1, 1)],
+       g.all_neighbors(0, 0)
+    );
+
+  }
 }
